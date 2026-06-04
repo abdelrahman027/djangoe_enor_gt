eab63a1f4493a9bd92b5969984b99b184e68b65c
abdelrahman027 <116889922+abdelrahman027@users.noreply.github.com>
2026-06-05 00:14:22 +0300

    edit settings

--- enor_core/settings.py
+++ enor_core/settings.py
@@ -241,7 +241,23 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+        },
+        "staticfiles" :{
+            "BACKEND":"django.contrib.staticfiles.storage.StaticFilesStorage"
+        }
+    }
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_ACCESS_SECRET_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_BUCKET_NAME')
+    # AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-east-1')
+    
+    # Tell Django to use S3 for Media Files
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
