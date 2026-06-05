bafd68a04e4e16d2fbb182162a6ce2af3cb9c63f
abdelrahman027 <116889922+abdelrahman027@users.noreply.github.com>
2026-06-05 10:58:43 +0300

    terms and orivacey and fix aws s3

--- enor_core/settings.py
+++ enor_core/settings.py
@@ -253,12 +253,21 @@ if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_ACCESS_SECRET_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_BUCKET_NAME')
+    # 1. Explicitly set the correct region for your bucket
+    AWS_S3_REGION_NAME = 'us-east-1' 
+    
+    # 2. Force Django to use Signature Version 4
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
     # AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', default='us-east-1')
     
     # Tell Django to use S3 for Media Files
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
-
+    # MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    # AWS_QUERYSTRING_AUTH = False
+    
+else:
+    MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
